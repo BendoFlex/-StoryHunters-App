a2f92f9bd745506772fef5b109bd90373a0aa7a6
bendoflex <joanzaf@lilo.org>
2020-10-15 00:32:42 +0200

    SearchFilm Logic start number of data fetch and loop in react

--- src/components/FilmSearch.js
+++ src/components/FilmSearch.js
@@ -5,29 +5,51 @@ class FilmSearch extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      value : ""
+      value : "",
+      films : [],
+      result : 0
     }
     this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this) //controlled component
   }
 
   handleSubmit(event){
     alert("fetch data from api : "+this.state.value)
+    this.setState({
+      'value': "",
+      'result' : 20
+    })
     event.preventDefault()
   }  
 
   handleChange(event){
     this.setState({'value': event.target.value})
-  }  
+  } 
+  
+  renderFilms(){
+     const item = []
+      for ( let i= 0; i<this.state.result;i++){
+        item.push(<div className="film_card"><h1 key={i} className="title">Film {i+1}</h1></div>)
+      }
+      return(
+        <div id="film_result">
+          {item}
+        </div>
+      )
+  }
 
   render(){
     return(
-      <form id="search" onSubmit={this.handleSubmit}>
-        <label>Search Film :
-          <input type="text" name="search" value={this.state.value} id="search" onChange={ this.handleChange}/>
-        </label>
-        <input type="submit" name="chercher"/>
-      </form>
+      <div id="main">
+        <form id="search" onSubmit={this.handleSubmit}>
+          <label>Search Film :
+            <input type="text" name="search" value={this.state.value} id="search" onChange={ this.handleChange}/>
+          </label>
+          <input type="submit" name="chercher"/>
+        </form>
+        {this.renderFilms()}
+      </div>
+      
     )
   }
 }
