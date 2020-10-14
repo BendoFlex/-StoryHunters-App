e0df4db3c76ca59682e77923ffed8e1c9a2872a4
bendoflex <joanzaf@lilo.org>
2020-10-14 23:59:29 +0200

    get the text passed in input

--- src/components/FilmSearch.js
+++ src/components/FilmSearch.js
@@ -4,15 +4,27 @@ import React from 'react'
 class FilmSearch extends React.Component {
   constructor(props){
     super(props)
+    this.state = {
+      value : ""
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  
+  handleSubmit(event){
+    alert("fetch data from api : "+this.state.value)
+    event.preventDefault()
+  }  
+
+  handleChange(event){
+    this.setState({'value': event.target.value})
+  }  
 
   render(){
     return(
       <form id="search" onSubmit={this.handleSubmit}>
         <label>Search Film :
-          <input type="text" name="search" id="search"/>
+          <input type="text" name="search" value={this.state.value} id="search" onChange={ this.handleChange}/>
         </label>
         <input type="submit" name="chercher"/>
       </form>
