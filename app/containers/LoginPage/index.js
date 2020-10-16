327de9f63edd0d22f5af5074f35a03909830dfc0
bendoflex <joanzaf@lilo.org>
2020-10-16 19:53:07 +0200

    changed login page with inputs

--- app/containers/LoginPage/index.js
+++ app/containers/LoginPage/index.js
@@ -1,21 +1,49 @@
 import React from 'react';
 
 class LoginPage extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      email : "",
+      password :''
+    }
+    this.handleTextChange = this.handleTextChange.bind(this)
+    this.handleConnexion = this.handleConnexion.bind(this)
+    
+  }
+
+  handleTextChange(event){
+    const text = event.target.value
+    if(event.target.name === "email"){
+      this.setState({email : text})
+    }
+    else {
+      this.setState({password : text})
+    }
+    
+  }
+
+  handleConnexion(event){
+    alert("captured email "+this.state.email+" and password "+this.state.password)
+    event.preventDefault();
+  }
+
+
   render() {
     return (
       <div id="loginPage">
-        <form>
+        <form onSubmit={this.handleConnexion}>
           <h1>Se connecter</h1>
           <p>
             <label>
               E-mail :
-              <input type="text" name="email" value="email" />
+              <input type="text" name="email" value={this.state.email} onChange={this.handleTextChange} />
             </label>
           </p>
           <p>
             <label>
               Password :
-              <input type="password" name="password" value="pwd" />
+              <input type="password" name="password" value={this.status.password} onChange={this.handleTextChange} />
             </label>
           </p>
 
