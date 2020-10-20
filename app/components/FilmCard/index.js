dbf8b6bfcb0dd910eff1927a00c562f72b9229ee
bendoflex <joanzaf@lilo.org>
2020-10-20 17:38:05 +0200

    Film Card as a fonctionnal comp

--- app/components/FilmCard/index.js
+++ app/components/FilmCard/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-export default class FilmCard extends React.Component {
-    render(){
+export default function FilmCard ({film}) {
         return(
             <div  className="filmCard">
-                <p><img src={"https://image.tmdb.org/t/p/w185/"+this.props.film.poster_path}/></p>
-                <h2>{this.props.film.title}</h2>
+                <p><img src={"https://image.tmdb.org/t/p/w185/"+film.poster_path}/></p>
+                <h2>{film.title}</h2>
             </div>
         )
-    }
 }
