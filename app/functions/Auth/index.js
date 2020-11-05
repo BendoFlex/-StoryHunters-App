5f7f3ca788c589ec5f8b924cdd98b61848beba49
bendoflex <joanzaf@lilo.org>
2020-11-05 01:22:47 +0100

    change in prod  in auth functions

--- app/functions/Auth/index.js
+++ app/functions/Auth/index.js
@@ -6,7 +6,7 @@ import "firebase/auth"; //works ? yes
 function googleSignIn(){
   let provider = new firebase.auth.GoogleAuthProvider();
 
-  firebase.auth().signInWithPopup(provider).then(function(result) {
+  firebase.auth().signInWithPopup(provider)/*.then(function(result) {
     // This gives you a Google Access Token. You can use it to access the Google API.
     var token = result.credential.accessToken;
     // The signed-in user info.
@@ -24,7 +24,7 @@ function googleSignIn(){
     var credential = error.credential;
     console.log(errorMessage)
     // ...
-  });
+  });*/
 }
 
 
